c4d06eda24ac7f0b07aa83083db3213abdcdaa47
nile27 <ddj03104@gmail.com>
2024-05-24 16:28:33 +0900

    JS: 01.변수, 02.자료형

---
+++ 1day/2.js
@@ -0,0 +1,33 @@
+let num1 = 10;
+let num2 = 10;
+
+// fruits1 === fruites2 = false
+// 값은 같지만 서로 다른 주소를 참조하기 때문.
+const fruits1 = ["apple", "banana"];
+const fruits2 = ["apple", "banana"];
+
+// Quiz
+
+// 1. 다음 변수들의 타입을 확인하여 콘솔에 출력하세요.
+let num = 42;
+let name = "John";
+let isTrue = true;
+let person = { name: "Alice", age: 30 };
+let fruits = ["apple", "banana", "orange"];
+console.log(
+  typeof num,
+  typeof name,
+  typeof isTrue,
+  typeof person,
+  typeof fruits
+);
+
+// 2. 다음 주석 친 부분에 주석 왼쪽에 있는 데이터의 자료형을 적어주세요.
+
+let length = 16; // number
+let lastName = "Brown"; // string
+let score = [10, 20]; // object(Array)
+let x = {
+  firstName: "Nick",
+  lastName: "Doe",
+}; // object
