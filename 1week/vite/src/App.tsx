041a8292b72c5329ad3433f428f93c5aeef04e55
nile27 <ddj03104@gmail.com>
2024-05-30 18:19:04 +0900

    vite, cra 환경설정

---
+++ 1week/vite/src/App.tsx
@@ -0,0 +1,14 @@
+import Button from "./components/Button";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <h1>Hello, React22</h1>
+      <Button />
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
